00ebaebf7f48f50007476be5c85669a8614954eb
RaySenior <bestmusicforwork@yandex.ru>
2019-12-18 18:11:10 +0300

    Added Cycle file

---
+++ Lesson1/Cycle.java
@@ -0,0 +1,27 @@
+public class Cycle {
+	
+	public static void main(String[] args) {
+		for(int i = 0; i <= 20; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+
+		int i = 6;
+		while(i >= -6) {
+			System.out.print(i + " ");
+			i -= 2;
+		}
+		System.out.println();
+
+		int number = 10;
+		int sum = 0;
+
+		do {
+			if(number % 2 != 0) {
+				sum += number;
+			}
+			number++;
+		} while(number < 21);
+		System.out.println("Summary of odd digits = " + sum);
+	}
+}
