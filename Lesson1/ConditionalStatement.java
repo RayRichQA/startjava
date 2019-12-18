8f3adef299da727b7ba47a8a4c1aabcb0f336271
RaySenior <bestmusicforwork@yandex.ru>
2019-12-18 17:05:34 +0300

    Removed empty lines after variables

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -1,24 +1,20 @@
 public class ConditionalStatement {
 	
 	public static void main(String[] args) {
-		int age = 21;
-		
+		int age = 21;		
 		if (age > 20) {
 			System.out.println("You're too old.");
 		}
 
-		boolean isMale = false;
-		
+		boolean isMale = false;		
 		if (isMale) {
 			System.out.println("You're a man.");
 		}
-
 		if (!isMale) {
 			System.out.println("Maybe you're a woman.");
 		}
 
 		float height = 2.20f;
-
 		if (height < 1.80) {
 			System.out.println("You're a cute gnome.");
 		} else {
@@ -26,7 +22,6 @@ public class ConditionalStatement {
 		}
 
 		char firstLetterName = 'I';
-
 		if (firstLetterName == 'M') {
 			System.out.println("You have to buy me a big chocolate box.");
 		} else if (firstLetterName == 'I') {
