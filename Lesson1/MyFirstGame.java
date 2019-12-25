2bc5a128ab862a6664edc92cfe8db17b74f0266e
RayRichQA <rayrichqa@gmail.com>
2019-12-25 12:21:25 +0300

    fixed MyFirstGame

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,15 +1,15 @@
 public class MyFirstGame {
 	
 	public static void main(String[] args) {
-		int number = 77;
+		int computerNumber = 77;
 		int userNumber = 75;
 
-		while(userNumber != number) {				
-			if(userNumber < number) {
-				System.out.println("Введенное Вами число меньше того, что загадал компьютер.");
+		while(userNumber != computerNumber) {				
+			if(userNumber < computerNumber) {
+				System.out.println("Введенное Вами число = " + userNumber + " меньше того, что загадал компьютер.");
 				userNumber += 4;
-			} else {
-				System.out.println("Введенное Вами число больше того, что загадал компьютер.");
+			} if(userNumber > computerNumber) {
+				System.out.println("Введенное Вами число = " + userNumber + " больше того, что загадал компьютер.");
 				userNumber -= 2;
 			}
 		}
