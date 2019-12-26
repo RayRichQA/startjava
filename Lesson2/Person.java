a7020f8b52816ffeb6f1f4eac59ec888e5bdb3c5
RayRichQA <rayrichqa@gmail.com>
2019-12-26 20:50:59 +0300

    Added Person

---
+++ Lesson2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	
+	String sex = "female";
+	String name = "Gnom";
+	float height = 101.5f;
+	float weight = 55.5f;
+	int age = 505;
+
+	void go() {
+		System.out.println("I can go.");
+	}
+
+	void sit() {
+		System.out.println("I can sit.");
+	}
+
+	void run() {
+		System.out.println("I can run.");
+	}
+
+	void speak() {
+		System.out.println("I can speak.");
+	}
+
+	void learnJava() {
+		System.out.println("Now I'm learning Java.");
+	}
+}
