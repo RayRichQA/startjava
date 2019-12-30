a8deb6dc2195529cf4f5f29f9db067a0c9f697b9
RayRichQA <rayrichqa@gmail.com>
2019-12-30 13:26:48 +0300

    Modified Jaeger after encapsulation lesson

--- Lesson2/Jaeger.java
+++ Lesson2/Jaeger.java
@@ -1,28 +1,44 @@
 public class Jaeger {
 	
-	String modelName = "Bracer Phoenix";
-	String mark = "Mark-5";
-	String origin = "USA";
-	float height = 70.7f;
-	float weight = 2.1f;
-	int speed = 3;
-	int strength = 8;
-	int armor = 9;
+	private String modelName = "Bracer Phoenix";
+	private String mark = "Mark-5";
+	private String origin = "USA";
+	private float height = 70.7f;
+	private float weight = 2.1f;
+	private int speed = 3;
+	private int strength = 8;
+	private int armor = 9;
 
-	boolean drift() {
+	public String getModelName() {
+		return modelName;
+	}
+
+	public void setModelName(String modelName) {
+		if(modelName == "") {
+			System.out.println("Пустая строка.");
+		} else {
+			this.modelName = modelName;
+		}		
+	}
+
+	public boolean drift() {
 		System.out.println("Вы вошли в дрифт");
 		return true;
 	}
 
-	void move() {
+	public void move() {
 		System.out.println("Вы прошли 5 шагов");
 	}
 
-	String scanKaiju() {
+	public String scanKaiju() {
 		return "nothing";
 	}
 
-	void useVortexCannon() {
+	public void useVortexCannon() {
 		
+	}
+
+	public String toString() {
+		return "modelName = " + modelName + " mark = " + mark;
 	}
 }
--- Lesson2/JaegerTest.java
+++ Lesson2/JaegerTest.java
@@ -2,9 +2,18 @@ public class JaegerTest {
 	
 	public static void main(String[] args) {
 		Jaeger jaegerOne = new Jaeger();
-		System.out.println("modelName = " + jaegerOne.modelName);
-		System.out.println("mark = " + jaegerOne.mark);
+		System.out.println("modelName = " + jaegerOne.getModelName());
+
+		jaegerOne.setModelName("Java");
+		System.out.println("modelName = " + jaegerOne.getModelName());
+
+		/*System.out.println("height = " + jaegerOne.height);
+		jaegerOne.height = -1;
+		System.out.println("height = " + jaegerOne.height);*/
+
+		/*System.out.println("mark = " + jaegerOne.mark);
 		System.out.println("speed = " + jaegerOne.speed);
 		System.out.println(jaegerOne.drift());
+		System.out.println(jaegerOne);*/
 	}
 }
